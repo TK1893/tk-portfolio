df7b48c193c9b71876a754c95804421b5fedaf3c
Tobias Kraft <155923508+TK1893@users.noreply.github.com>
2024-11-19 13:03:03 +0100

    Update Ex-6-6

---
+++ src/js/kraftchat.js
@@ -0,0 +1,4 @@
+// src\js\meet.js
+import * as bootstrap from 'bootstrap';
+// import '../scss/navbar-footer.scss';
+import '../scss/code.scss';
---
+++ src/js/kraftflix-react.js
@@ -0,0 +1,4 @@
+// Import aller Bootstrap-JS
+import * as bootstrap from 'bootstrap';
+import '../scss/code.scss';
+// import '../scss/navbar-footer.scss';
